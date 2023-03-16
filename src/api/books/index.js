a4caf211e2bd3be523087e37b2b069a71f82a7d6
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-03-16 12:07:53 +0100

    Adds mongoose custom method example

--- src/api/books/index.js
+++ src/api/books/index.js
@@ -21,13 +21,7 @@ booksRouter.get("/", async (req, res, next) => {
     console.log("q2m:", q2m(req.query))
     const mongoQuery = q2m(req.query)
     //  price: '>10' should be converted somehow into price: {$gt: 10}
-    const books = await BooksModel.find(mongoQuery.criteria, mongoQuery.options.fields)
-      .limit(mongoQuery.options.limit)
-      .skip(mongoQuery.options.skip)
-      .sort(mongoQuery.options.sort)
-      .populate({ path: "authors", select: "firstName lastName" })
-    const total = await BooksModel.countDocuments(mongoQuery.criteria)
-    // no matter the order of usage of these methods, Mongo will ALWAYS apply SORT then SKIP then LIMIT
+    const { books, total } = await BooksModel.findBooksWithAuthors(mongoQuery)
     res.send({
       links: mongoQuery.links("http://localhost:3001/books", total),
       total,
--- src/api/books/model.js
+++ src/api/books/model.js
@@ -14,4 +14,18 @@ const booksSchema = new Schema(
   { timestamps: true }
 )
 
+// *************************** MODEL CUSTOM METHOD **********************
+
+booksSchema.static("findBooksWithAuthors", async function (query) {
+  console.log("THIS: ", this)
+  const books = await this.find(query.criteria, query.options.fields)
+    .limit(query.options.limit)
+    .skip(query.options.skip)
+    .sort(query.options.sort)
+    .populate({ path: "authors", select: "firstName lastName" })
+  const total = await this.countDocuments(query.criteria)
+  // no matter the order of usage of these methods, Mongo will ALWAYS apply SORT then SKIP then LIMIT
+  return { books, total }
+})
+
 export default model("Book", booksSchema)
